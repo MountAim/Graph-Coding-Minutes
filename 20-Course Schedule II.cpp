191c1190400111afc0bb12d6e151335b7dc1cb25
Siddharth Singhal <47295341+ssid8802@users.noreply.github.com>
2021-11-23 16:37:01 +0530

    Update 20-Course Schedule II.cpp

--- 20-Course Schedule II.cpp
+++ 20-Course Schedule II.cpp
@@ -1,51 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> findOrder(int numCourses, vector<vector<int>> prerequisites) {
-    vector<vector<int>>v(numCourses+1);
-    int x,y,val;
-    vector<int>ind(numCourses+1);
-    for(int i=0;i<prerequisites.size();i++)
-    {
-        x=prerequisites[i][0],y=prerequisites[i][1];
-        v[y].push_back(x);
-        ind[x]++;
-    }
-    queue<int>q;
-    vector<int>topo;
-    set<int>s;
-    for(int i=0;i<numCourses;i++)
-    {
-        if(ind[i]==0)
-        {
-            s.insert(i);
-        }
-    }
-    for(auto itr:s)
-    {
-        q.push(itr);
-    }
-    while(q.size())
-    {
-        x=q.front();
-        q.pop();
-        topo.push_back(x);
-        s.clear();
-        for(auto itr:v[x])
-        {
-            ind[itr]--;
-            if(ind[itr]==0){
-                s.insert(itr);
-            }
-        }
-        for(auto itr:s)
-        {
-            q.push(itr);
-        }
-    }
-    if(topo.size()!=numCourses)
-    {
-        return {};
-    }
-    return topo;
+bool canFinish(vector<vector<int>> prerequisites, int n) {
+    vector<vector<int>> G(n);
+    vector<int> degree(n, 0), bfs;
+    for (auto& e : prerequisites)
+        G[e[1]].push_back(e[0]), degree[e[0]]++;
+    for (int i = 0; i < n; ++i) if (!degree[i]) bfs.push_back(i);
+    for (int i = 0; i < bfs.size(); ++i)
+        for (int j: G[bfs[i]])
+            if (--degree[j] == 0) bfs.push_back(j);
+    return bfs.size() == n;
 }
